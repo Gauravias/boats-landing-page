1ea9a74fd483610fdfeee5263c1a87eacae0a4cf
Gaurav Tripathi <>
2024-04-06 09:59:29 +0530

    n

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      transitionProperty: {
+        'text': 'font-size',
+      },
+      screens: {
+        '593': '593px',
+        '948': '948px',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
